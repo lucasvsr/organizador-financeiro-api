f703236ddd5196bb28bb13f086fcd6f60c7f2021
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2020-07-16 14:38:43 -0300

    Criação do projeto

---
+++ src/test/java/com/br/lvsribeiro/organizadorfinanceiroapi/OrganizadorFinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.lvsribeiro.organizadorfinanceiroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizadorFinanceiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
